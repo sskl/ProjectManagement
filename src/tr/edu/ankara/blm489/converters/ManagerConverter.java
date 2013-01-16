6eba6e880d43c7a127a6d87736dd0a4387f4d45c
basar <turgutbasar@gmail.com>
2013-01-16 23:22:05 +0200

    Project Adddition and Manager Converted has been repaired.

--- src/tr/edu/ankara/blm489/converters/ManagerConverter.java
+++ src/tr/edu/ankara/blm489/converters/ManagerConverter.java
@@ -16,52 +16,34 @@ import tr.edu.ankara.blm489.models.Manager;
  * @author sskl
  * 
  */
+
 public class ManagerConverter implements Converter {
 
-	/**
-	 * 
-	 */
-	public ManagerConverter() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext
-	 * , javax.faces.component.UIComponent, java.lang.String)
-	 */
 	@Override
-	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2)
+	public Object getAsObject(FacesContext context, UIComponent component, String value)
 			throws ConverterException {
+		if (value == null)
+			return null;
 		try {
 			EntityManager em = MainControl.getEmf().createEntityManager();
-			int id = Integer.parseInt(arg2);
+			int id = Integer.parseInt(value);
 			Manager m = em.find(Manager.class, id);
 			em.getTransaction().begin();
 			em.getTransaction().commit();
 			return m;
 		} catch (Exception e) {
-			System.out.println(e.getMessage() + arg2);
+			System.out.println(e.getMessage() + value);
 		}
 
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext
-	 * , javax.faces.component.UIComponent, java.lang.Object)
-	 */
 	@Override
-	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2)
+	public String getAsString(FacesContext context, UIComponent component, Object value)
 			throws ConverterException {
-		Manager m = (Manager) arg2;
-
-		return (m.getId() != 0) ? String.valueOf(m.getId()) : null;
+		Manager m = (Manager)value; 
+		return m.toString();
+		
 	}
 
 }
--- src/tr/edu/ankara/blm489/models/User.java
+++ src/tr/edu/ankara/blm489/models/User.java
@@ -118,4 +118,11 @@ public class User {
 	public void setRole(Role role) {
 		this.role = role;
 	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(id);
+	}
+	
+	
 }
